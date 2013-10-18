8c6ae6fe395e4e0511906d6be6813ccad734f1d3
mnovytskyi <nikola.novitski@gmail.com>
2013-10-18 19:06:05 +0300

    third comment

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World!");
+    response.write("Hello World!It's my first node app");
     response.end();
 }).listen(process.env.PORT || 8888);
