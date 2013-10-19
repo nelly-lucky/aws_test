b14ab8ba8c2e994565ce88a035699d0339d31fa6
mnovytskyi <nikola.novitski@gmail.com>
2013-10-19 09:14:56 +0300

    test2

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
  * Created by mnovytskyi on 10/18/13.
  */
 var http = require("http");
+var test = require('test');
+test();
 
 http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World!<p>Gor is the very good programmer!</p>");
-    console.log("Okay");
     console.log("Okay");
     response.end();
 }).listen(process.env.PORT || 8888);
