c1f05ac4cd2badc919ddcc726754b2a856eb5922
mnovytskyi <nikola.novitski@gmail.com>
2013-10-19 10:39:21 +0300

    env PORT

--- server.js
+++ server.js
@@ -9,5 +9,6 @@ http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World!<p>Gor is the very good programmer!</p>");
     console.log("Okay");
+    console.log("Okay", process.env.PORT);
     response.end();
 }).listen(process.env.PORT || 8888);
